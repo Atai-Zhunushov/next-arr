acbaf12500c39f645083792df90afc86365443fd
Atai-Zhunushov <ataizhunushov@mail.ru>
2023-12-24 22:53:26 +0600

    users page add type

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,10 +3,16 @@ import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 import {List, ListItem} from "@mui/material";
 
+interface User {
+    id: number;
+    name: string;
+    // Добавьте другие поля пользователя, если они имеются
+}
 
 
-const Users = () => {
-    const [usersArr , setUsers] = useState([])
+
+const Users: React.FC = () => {
+    const [usersArr, setUsers] = useState<User[]>([]);
     useEffect(() => {
         const getUsers = async () => {
             const response = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -19,13 +25,18 @@ const Users = () => {
     return (
             <div className='flex justify-center'>
                 <List>
-                    {usersArr.map(user =>
-                        <ListItem key={user.id} className='no-underline'>
-                            <Link href={`/users/${user.id}`}>
-                                {user.name}
-                            </Link>
-
-                        </ListItem>
+                    {usersArr.length > 0 ? (
+                        <List>
+                            {usersArr.map((user: User) => (
+                                <ListItem key={user.id} className='no-underline'>
+                                    <Link href={`/users/${user.id}`}>
+                                        {user.name}
+                                    </Link>
+                                </ListItem>
+                            ))}
+                        </List>
+                    ) : (
+                        <p>Loading...</p>
                     )}
                 </List>
             </div>
